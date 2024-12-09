1564a48bced9f76614efcd9147be5b3c9fd3bc98
3m0l3y <allan.emily@gmail.com>
2024-12-09 07:10:17 -0800

    text_node_to_html_node incomplete

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 
 from textnode import TextNode
 from textnode import TextType
+from textnode import text_node_to_html_node
 
 
 def main():
     # hello world
     print("hello world")
     test = TextNode("Test text in main.", TextType.BOLD, "https://www.dummydomain.com")
+    test_text = "This is test text in the main.py file."
+    text_node_to_html_node(test_text)
     print(test)
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -39,9 +39,9 @@ def text_node_to_html_node(text_node):
     text_type = text_node.text_type
     print(text_type)
     if text_node.url is not None:
-        # do things
+        print(text_node.url) # debug
     if text_node.alt is not None:
-        # do things
+        print(text_node.alt) # debug
 
     
 
