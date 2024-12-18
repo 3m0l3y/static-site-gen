27335ffdb74e4888d3f84e78e861b208bf34367f
3m0l3y <allan.emily@gmail.com>
2024-12-18 11:25:23 -0800

    working on textnode function

--- src/textnode.py
+++ src/textnode.py
@@ -35,10 +35,26 @@ class TextNode:
 # convert a TextNode to an HTMLNode. # Not complete
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.NORMAL:
-        # handle TEXT type
+        # handle NORMAL type
         print(text_node.text_type)
     elif text_node.text_type == TextType.BOLD:
         # handle BOLD type
+        # and so on for other TextTypes...
+        print(text_node.text_type)
+    elif text_node.text_type == TextType.ITALIC:
+        # handle ITALIC type
+        # and so on for other TextTypes...
+        print(text_node.text_type)
+    elif text_node.text_type == TextType.CODE:
+        # handle CODE type
+        # and so on for other TextTypes...
+        print(text_node.text_type)
+    elif text_node.text_type == TextType.LINK:
+        # handle LINK type
+        # and so on for other TextTypes...
+        print(text_node.text_type)
+    elif text_node.text_type == TextType.IMAGE:
+        # handle IMAGE type
         # and so on for other TextTypes...
         print(text_node.text_type)
     else:
